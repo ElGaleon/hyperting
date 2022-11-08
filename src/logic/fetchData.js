01ed7b2971691964a53c15f4e1b3b38debeb75ac
Christian Galeone <61737206+ElGaleon@users.noreply.github.com>
2022-11-09 00:38:36 +0100

    Hyperting Junior Frontend Test

--- src/logic/fetchData.js
+++
@@ -1,9 +0,0 @@
-const api = 'https://gorest.co.in/public/v2/users';
-
-export async function fetchData () {
-    return await fetch(api, {method: 'GET', headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-          }}).then((response) => response.json())    
-            .catch((error) => console.log(error));
-}
